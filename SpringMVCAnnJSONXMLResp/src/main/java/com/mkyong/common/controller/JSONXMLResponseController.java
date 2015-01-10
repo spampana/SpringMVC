0342dd377b3a2ee434193a378b124bb8d5c06ab4
satya <satya.iton@gmail.com>
2015-01-10 03:00:39 -0800

    Spring MVC Annotation with XML and JSON Response

--- SpringMVCAnnJSONXMLResp/src/main/java/com/mkyong/common/controller/JSONXMLResponseController.java
+++ SpringMVCAnnJSONXMLResp/src/main/java/com/mkyong/common/controller/JSONXMLResponseController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mkyong.common.model.Coffee;
 import com.mkyong.common.model.Shop;
 
 @Controller
-@RequestMapping("/kfc/brands")
-public class JSONController {
+@RequestMapping("/kfc")
+public class JSONXMLResponseController {
 
-	@RequestMapping(value = "{name}", method = RequestMethod.GET)
+	@RequestMapping(value = "/JSON/{name}", method = RequestMethod.GET ,headers = "Accept=application/json")
 	public @ResponseBody
 	Shop getShopInJSON(@PathVariable String name) {
 
@@ -20,6 +22,18 @@ public class JSONController {
 		shop.setStaffName(new String[] { "mkyong1", "mkyong2" });
 
 		return shop;
+
+	}
+	
+	@RequestMapping(value = "/XML/{name}", method = RequestMethod.GET ,headers = "Accept=application/xml")
+	public @ResponseBody
+	Coffee getShopInXML(@PathVariable String name) {
+
+		Coffee coff= new Coffee();
+		coff.setName(name);
+		coff.setQuanlity(2);
+
+		return coff;
 
 	}
 
